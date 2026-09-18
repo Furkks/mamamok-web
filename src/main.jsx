15311728062680827dbfc00b966b58b889a83f5b
Furkan Aras <furkan.aras@etudiant.univ-rennes1.fr>
2026-09-18 20:28:58 +0200

    Route mentions légales

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AdminPanel from "./AdminPanel";
+import MentionsLegales from "./MentionsLegales";
 
 const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  path === "/admin" ? <AdminPanel /> : <App />
+  path === "/admin" ? <AdminPanel /> :
+  path === "/mentions-legales" ? <MentionsLegales /> :
+  <App />
 );
